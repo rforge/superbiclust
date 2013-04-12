9836569b0b80e0a9fbbec37160417ca8a087ee08
thamiakova <thamiakova@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-12 19:20:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/superbiclust@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/superbiclust/R/biclustData-class.R
+++ pkg/superbiclust/R/biclustData-class.R
@@ -41,3 +41,58 @@ setMethod("BiclustSet",signature("list"),
 			myBiclustSet = new("BiclustSet",GenesMembership=bicArows, ColumnMembership=bicAcols, 
 					Number=biclust1Number)
 		})
+###**show and summary teh same as for Biclust class******************************
+
+setMethod("show", "BiclustSet",
+		function(object)
+		{
+			cat("\nAn object of class",class(object),"\n\n")			
+			n<-object@Number
+			n<-min(c(n,5))
+			if(n>1)
+			{
+				cat("\nNumber of Clusters found: ",object@Number, "\n")
+				cat("\nFirst ",n," Cluster sizes:\n")
+				
+				rowcolsizes<-rbind(colSums(object@GenesMembership[,1:n]),rowSums(object@ColumnMembership[1:n,]))
+				rownames(rowcolsizes)<-c("Number of Rows:","Number of Columns:")
+				colnames(rowcolsizes)<-paste("BC", 1:n)
+				#print.default(format(rowcolsizes, print.gap = 2, quote = FALSE))
+				print(rowcolsizes)
+			}
+			else
+			{
+				if(n==1) cat("\nThere was one cluster found with\n ",sum(object@GenesMembership[,1]), "Rows and ", sum(object@ColumnMembership), "columns")
+				if(n==0) cat("\nThere was no cluster found")
+			}
+			cat("\n\n")
+		})
+
+setGeneric("summary")
+setMethod("summary", "BiclustSet",
+		function(object)
+		{
+			cat("\nAn object of class",class(object),"\n\n")
+			n<-object@Number
+			
+			if(n>1)
+			{
+				cat("\nNumber of Clusters found: ",object@Number, "\n")
+				cat("\nCluster sizes:\n")
+				
+				rowcolsizes<-rbind(colSums(object@GenesMembership[,1:n]),rowSums(object@ColumnMembership[1:n,]))
+				rownames(rowcolsizes)<-c("Number of Rows:","Number of Columns:")
+				colnames(rowcolsizes)<-paste("BC", 1:n)
+				#print.default(format(rowcolsizes, print.gap = 2, quote = FALSE))
+				print(rowcolsizes)
+			}
+			else
+			{
+				if(n==1) cat("\nThere was one cluster found with\n ",sum(object@GenesMembership[,1]), "Rows and ", sum(object@ColumnMembership), "columns")
+				if(n==0) cat("\nThere was no cluster found")
+			}
+			cat("\n\n")
+			
+			
+		})
+
